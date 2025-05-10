30358b315bf3d669f6ebc47eacf5d07abee38f3e
Ilman <dzubyan04@gmail.com>
2025-05-10 17:55:17 +0700

    feature: Added recipe value wanted when choosing multiple recipe

--- src/frontend/src/app/components/burgermenu.jsx
+++ src/frontend/src/app/components/burgermenu.jsx
@@ -4,6 +4,15 @@ import './burgermenu.css';
 function BurgerMenu() {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("Shortest");
+    const [customValue, setCustomValue] = useState("");
+
+    const options = ["Shortest", "Multiple"];
+    const isLastOptionSelected = selectedOption === options[options.length - 1];
+
+    const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
     return (
     <>
@@ -23,10 +32,26 @@ function BurgerMenu() {
         </select>
 
         <label>Option:</label>
-        <select className="menu-select">
-          <option>Shortest Recipe</option>
-          <option>Multiple Recipe</option>
+        <select className="menu-select" onChange={handleSelectChange}>
+          {options.map((opt, idx) => (
+            <option key={idx} value={opt}>
+              {opt + " Recipe"}
+            </option>
+          ))}
         </select>
+
+        {isLastOptionSelected && (
+        <div className="multiple-extra-option">
+          <label>
+            How many recipes ?
+            <input className = "value-form"
+              type="number"
+              value={customValue}
+              onChange={(e) => setCustomValue(e.target.value)}
+            />
+          </label>
+        </div>
+      )}
       </div>
 
       {isOpen && <div className="overlay" onClick={() => setIsOpen(false)} />}
